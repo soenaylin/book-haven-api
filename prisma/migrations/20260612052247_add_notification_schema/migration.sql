6a1b4314f35316321f7850267885a865fcf2089f
Soe Nay Lin <soenaylin.g@gmail.com>
2026-06-12 05:26:02 +0000

    feat(noti): create notification schema

---
+++ prisma/migrations/20260612052247_add_notification_schema/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+    "link" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
